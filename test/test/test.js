06d851b24c92911e0a9bd17586b0f751b8b98b62
oyps <ponconsoft@gmail.com>
2023-05-07 00:46:51 +0800

    ...

--- test/test/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var main_1 = require("../src/main");
-var router = new main_1.default();
-router.set(['home', 'list', 'about', 'video', 'share']);
-router.start();
-window['router'] = router;
-window['ApeeRouter'] = main_1.default;
